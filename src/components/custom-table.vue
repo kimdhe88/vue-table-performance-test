626daea86ac878b21c67cb16979daacb71d82097
kimdhe88 <41457962+kimdhe88@users.noreply.github.com>
2020-08-10 18:23:33 +0900

    커스텀 테이블 생성 중

--- src/components/custom-table.vue
+++ src/components/custom-table.vue
@@ -1,21 +1,264 @@
 <template>
-  <div>
-    <table class="table table-dark" border="2px">
+  <div class="custom-table">
+    <table border="1px">
       <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Name</th>
-          <th scope="col">Department</th>
-        </tr>
+        <div>
+          <tr>
+            <th
+              class="custom-cell"
+              v-for="header in headers"
+              v-bind:key="header._colid"
+            >{{header.text}}</th>
+          </tr>
+        </div>
       </thead>
-      <tbody>
-        <tr v-for="(emp,index) in employee" :key="index" :employee="employee">
-          <td>{{ emp.id }}</td>
-          <td>{{ emp.name }}</td>
-          <td>{{ emp.department }}</td>
-        </tr>
-      </tbody>
+      <tr>
+        <div v-for="item in draw.drawItems" v-bind:key="item._rowid">
+          <td
+            class="custom-cell"
+            @mousedown="cellMouseDown(item._rowid, header._colid)"
+            @mouseover="cellIndexTracking(item._rowid, header._colid)"
+            @mouseup="cellMouseUp(item._rowid, header._colid)"
+            v-for="header in headers"
+            v-bind:key="header._colid"
+            :class="item[header._colid].isSelected ? 'select' : 'non-select'"
+          >{{item[header._colid].value}}</td>
+        </div>
+      </tr>
     </table>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["headers", "items"],
+
+  data() {
+    return {
+      isTracking: false,
+      selectPoint: {
+        down: { rowid: "", colid: "" },
+        over: { rowid: "", colid: "" },
+      },
+      draw: {
+        items: "",
+        drawItems: "",
+        beginIndex: 0,
+        maximumBeginIndex: 0,
+        drawCount: 15,
+      },
+      selectedCell: { rowid: "", colid: "" },
+      selectedCells: [],
+      selectCells: [],
+    };
+  },
+
+  watch: {
+    headers: function (givenHeader) {
+      this.setHeaderOrdering(givenHeader);
+    },
+    items: function (newItems) {
+      this.draw.items = newItems;
+      this.drawItems();
+    },
+  },
+
+  methods: {
+    async cellIndexTracking(rowid, colid) {
+      if (!this.isTracking) return;
+      this.selectPoint.over.rowid = rowid;
+      this.selectPoint.over.colid = colid;
+      this.AddToSelectVector();
+    },
+
+    async cellMouseDown(rowid, colid) {
+      this.isTracking = true;
+      this.selectPoint.down.rowid = rowid;
+      this.selectPoint.down.colid = colid;
+      this.selectedCell = this.selectPoint.down;
+      this.cellIndexTracking(rowid, colid);
+    },
+
+    async cellMouseUp(idx, entry) {
+      this.isTracking = false;
+    },
+
+    async AddToSelectVector() {
+      // console.time("Extract selected cells");
+      let beginRowid = 0;
+      let endRowId = 0;
+      let beginColId = 0;
+      let endColId = 0;
+      if (this.selectPoint.down.rowid < this.selectPoint.over.rowid) {
+        beginRowid = this.selectPoint.down.rowid;
+        endRowId = this.selectPoint.over.rowid;
+      } else {
+        beginRowid = this.selectPoint.over.rowid;
+        endRowId = this.selectPoint.down.rowid;
+      }
+
+      if (this.selectPoint.down.colid < this.selectPoint.over.colid) {
+        beginColId = this.selectPoint.down.colid;
+        endColId = this.selectPoint.over.colid;
+      } else {
+        beginColId = this.selectPoint.over.colid;
+        endColId = this.selectPoint.down.colid;
+      }
+
+      console.log(`beginRowid : ${beginRowid}, endRowId : ${endRowId}`);
+      console.log(`beginColId : ${beginColId}, endColId : ${endColId}`);
+
+      this.selectCells = new Array();
+      for (let rowid = beginRowid; rowid <= endRowId; rowid++) {
+        for (let colid = beginColId; colid <= endColId; colid++) {
+          this.selectCells.push({ rowid, colid });
+          this.draw.drawItems[rowid][colid].isSelected = true;
+        }
+      }
+
+      for (let idx in this.selectedCells) {
+        let isFound = false;
+        for (let inIdx in this.selectCells) {
+          if (
+            parseInt(this.selectedCells[idx].rowid) ===
+              parseInt(this.selectCells[inIdx].rowid) &&
+            parseInt(this.selectedCells[idx].colid) ===
+              parseInt(this.selectCells[inIdx].colid)
+          ) {
+            isFound = true;
+            break;
+          }
+        }
+        if (!isFound) {
+          this.draw.drawItems[this.selectedCells[idx].rowid][
+            this.selectedCells[idx].colid
+          ].isSelected = false;
+        }
+      }
+      console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+      // console.log("difference");
+      // console.log(difference);
+
+      this.selectedCells = this.selectCells;
+      // console.timeEnd("Extract selected cells");
+    },
+
+    async drawItems(moveIdx = 0) {
+      console.log(`draw items : ${moveIdx}`);
+      this.draw.beginIndex += moveIdx;
+      if (moveIdx === 0 || this.draw.beginIndex < 0) this.draw.beginIndex = 0;
+      this.draw.maximumBeginIndex =
+        this.items.length - this.draw.drawCount < 0
+          ? 0
+          : this.items.length - this.draw.drawCount;
+      if (this.draw.beginIndex > this.draw.maximumBeginIndex)
+        this.draw.beginIndex = this.draw.maximumBeginIndex;
+
+      console.log(`this.draw.beginIndex : ${this.draw.beginIndex}`);
+      console.log(
+        `this.draw.maximumBeginIndex : ${this.draw.maximumBeginIndex}`
+      );
+
+      let itemChunk = this.draw.items.slice(
+        this.draw.beginIndex,
+        this.draw.beginIndex + this.draw.drawCount
+      );
+
+      let buffers = new Array();
+      for (let idx in itemChunk) {
+        let tmpRows = new Object();
+        tmpRows._rowid = parseInt(this.draw.beginIndex + idx);
+        for (let hidx in this.headers) {
+          tmpRows[this.headers[hidx]._colid] = new Object();
+          tmpRows[this.headers[hidx]._colid].value =
+            itemChunk[idx][this.headers[hidx].value];
+          tmpRows[this.headers[hidx]._colid].isSelected = await this.isSelected(
+            tmpRows._rowid,
+            this.headers[hidx]._colid
+          );
+        }
+        buffers.push(tmpRows);
+      }
+
+      // console.log(buffers);
+      this.draw.drawItems = buffers;
+    },
+
+    async isSelected(rowid, colid) {
+      // console.log(`isSelected`);
+      let isSelectedCell = await this.selectedCells.includes({ rowid, colid });
+      return isSelectedCell;
+    },
+
+    getColumnName(colid) {
+      let columnName = "";
+      for (let idx in this.headers)
+        if (this.headers[idx]._colid === colid)
+          columnName = this.headers[idx].value;
+      return columnName;
+    },
+
+    setHeaderOrdering(givenHeader) {
+      for (let idx in givenHeader) this.headers[idx]._colid = idx;
+      // console.log(this.headers);
+    },
+  },
+
+  mounted() {
+    window.addEventListener("keydown", (event) => {
+      // if (!this.isObject) return; // 객체 선택 상태가 아니라면 패스
+
+      if (event.keyCode === 33) {
+        // console.log("Page up");
+        event.preventDefault();
+        if (event.altKey) this.drawItems(-this.drawCount);
+        if (event.shiftKey) this.drawItems(-1000);
+        else this.drawItems(-16);
+        event.initEvent();
+      }
+      if (event.keyCode === 34) {
+        // console.log("Page down");
+        event.preventDefault();
+        if (event.altKey) this.drawItems(this.drawCount);
+        else if (event.shiftKey) this.drawItems(1000);
+        else this.drawItems(16);
+
+        // event.initEvent();
+      }
+      if (event.keyCode === 38) {
+        // console.log("Arrow up");
+        event.preventDefault();
+        if (event.ctrlKey) this.drawItems(-this.drawCount);
+        else if (event.shiftKey) this.drawItems(-1000);
+        else this.drawItems(-3);
+        // event.initEvent();
+      }
+      if (event.keyCode === 40) {
+        // console.log("Arrow down");
+        event.preventDefault();
+        if (event.ctrlKey) this.drawItems(this.drawCount);
+        else if (event.shiftKey) this.drawItems(1000);
+        else this.drawItems(3);
+        // event.initEvent();
+      }
+    });
+  },
+};
+</script>
+
+<style >
+.custom-cell {
+  overflow-y: hidden !important;
+  overflow-x: hidden !important;
+  white-space: nowrap !important;
+  user-select: none;
+  min-width: 100px;
+  max-width: 100px;
+}
+
+.select {
+  background-color: brown;
+}
+</style>
 
--- src/views/extra-option.vue
+++ src/views/extra-option.vue
@@ -8,7 +8,7 @@
               v-on:keyup.enter="getData"
               v-model="getRows"
               :counter="10"
-              label="get rows"
+              label="조회 건수"
               required
             ></v-text-field>
           </v-col>
@@ -35,25 +35,14 @@
         </v-row>
         <v-row>
           <v-col cols="12" md="12">
-            <div id="scroll-table">
-              <v-data-table
-                dense
-                :height="400"
-                :fixed-header="true"
-                :loading="isLoading"
-                :headers="headers"
-                :items="drawData"
-                :items-per-page="drawRowSize"
-                class="elevation-1"
-              ></v-data-table>
+            <div>
+              <CustomTable :headers="headers" :items="items"></CustomTable>
             </div>
           </v-col>
         </v-row>
 
         <v-row>
-          <v-col>
-            <v-slider v-model="beginIndex" :max="maximumBeginIndex" step="1"></v-slider>
-          </v-col>
+          <v-col></v-col>
         </v-row>
       </v-container>
     </v-form>
@@ -63,39 +52,21 @@
 <script>
 import axios from "axios";
 import db from "../modules/database";
-
-let database = new Array();
-let vDatabase = new Array();
+import CustomTable from "@/components/custom-table";
 
 export default {
   name: "App",
 
-  components: {},
+  components: { CustomTable },
 
   data() {
     return {
       isLoading: false,
       getRows: 500,
-      beginIndex: 0,
-      fetchCount: 0,
-      drawCount: 0,
-      drawData: [{ sample: null }],
-      drawRowSize: 10,
-      headers: [{ text: "sample", value: "sample" }],
-      execTime: 0,
-      search: "",
+      items: "",
+      drawRowSize: 15,
+      headers: "",
     };
-  },
-
-  watch: {
-    beginIndex: function (newIndex) {
-      // console.log(`newIndex : ${newIndex}`);
-      this.refreshDrawData();
-    },
-    search: function (newText) {
-      // console.log(`newIndex : ${newIndex}`);
-      // this.tableSearch(newText);
-    },
   },
 
   methods: {
@@ -105,118 +76,33 @@ export default {
       this.isLoading = true;
       let data = await db.getTableByReturnType("object", this.getRows);
       this.headers = await this.getHeaders(data.fields);
-      this.fetchCount = data.rowCount;
-      this.drawCount = data.rowCount;
       // console.log(this.headers);
-      database = data.rows;
-      vDatabase = database;
+      this.items = data.rows;
 
-      this.execTime = data.execTime;
-      // console.log(this.headers);
-      this.setBeginIndex();
-      this.setMaximumBeginIndex();
-      this.refreshDrawData();
       this.isLoading = false;
     },
 
     async getHeaders(fields = Array) {
-      // console.log(fields);
       let headers = new Array();
       for (let idx = 0; idx < fields.length; idx++) {
         headers[idx] = new Object();
         headers[idx].text = fields[idx];
         headers[idx].value = fields[idx];
+        headers[idx].width = "50px";
+        headers[idx].fixed = true;
       }
-      // console.log(headers);
-
       return headers;
     },
-
-    async setBeginIndex() {
-      this.beginIndex = 0;
-    },
-
-    async setMaximumBeginIndex() {
-      this.maximumBeginIndex = vDatabase.length - this.drawRowSize;
-      if (this.maximumBeginIndex < 0) this.maximumBeginIndex = 0;
-      console.log(`this.maximumBeginIndex : ${this.maximumBeginIndex}`);
-    },
-
-    async moveBeginIndex(num) {
-      this.beginIndex += num;
-
-      if (this.beginIndex <= 0) this.beginIndex = 0;
-      if (this.beginIndex > this.maximumBeginIndex)
-        this.beginIndex = this.maximumBeginIndex;
-    },
-
-    async refreshDrawData() {
-      // this.drawData = database.slice(this.beginIndex, this.beginIndex + 10);
-      this.drawData = vDatabase.slice(this.beginIndex, this.beginIndex + 10);
-    },
-
-    // async refreshDrawData() {
-    //   let tmp = new Array();
-    //   for (let i = this.beginIndex; i < this.beginIndex + 10; i++)
-    //     tmp.push(vDatabase[i]);
-    //   this.drawData = tmp;
-    // },
-
-    async tableSearch(searchText) {
-      console.log(`table search`);
-
-      vDatabase = database.filter((obj) =>
-        Object.values(obj).some((val) =>
-          val ? val.toString().toLowerCase().includes(searchText) : false
-        )
-      );
-      this.drawCount = vDatabase.length;
-      this.setMaximumBeginIndex();
-      this.refreshDrawData();
-    },
-
-    async tableOrdering() {
-      database.sort(function (a, b) {
-        return a.seq < b.seq ? -1 : a.seq > b.seq ? 1 : 0;
-      });
-    },
-  },
-
-  mounted() {
-    window.addEventListener("keydown", (event) => {
-      // event.preventDefault();
-      // console.log(`event.keyCode : ${event.keyCode}`);
-      // console.log();
-      if (event.keyCode === 33) {
-        // console.log("Page up");
-        event.preventDefault();
-        if (event.altKey) this.moveBeginIndex(-this.drawCount);
-        if (event.shiftKey) this.moveBeginIndex(-1000);
-        else this.moveBeginIndex(-10);
-        event.initEvent();
-      }
-      if (event.keyCode === 34) {
-        // console.log("Page down");
-        event.preventDefault();
-        if (event.altKey) this.moveBeginIndex(this.drawCount);
-        else if (event.shiftKey) this.moveBeginIndex(1000);
-        else this.moveBeginIndex(10);
-
-        event.initEvent();
-      }
-      if (event.keyCode === 38) {
-        // console.log("Arrow up");
-        event.preventDefault();
-        this.moveBeginIndex(-3);
-        event.initEvent();
-      }
-      if (event.keyCode === 40) {
-        // console.log("Arrow down");
-        event.preventDefault();
-        this.moveBeginIndex(3);
-        event.initEvent();
-      }
-    });
   },
 };
 </script>
+
+
+<style>
+.text-start {
+  overflow-y: hidden !important;
+  overflow-x: hidden !important;
+  white-space: nowrap !important;
+  max-width: 200px !important;
+}
+</style>
